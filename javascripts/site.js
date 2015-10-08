e77edae0fcd429505d0de53130ffe6a3976c1385
Jessica Dear <jessicamdearr@gmail.com>
2015-10-07 21:25:04 -0700

    stuff

--- javascripts/site.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {  
-   $("#homepage-carousel").swiperight(function() {  
-      $("#homepage-carousel").carousel('prev');  
-    });  
-   $("#homepage-carousel").swipeleft(function() {  
-      $("#homepage-carousel").carousel('next');  
-   });  
-});
